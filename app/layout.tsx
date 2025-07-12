4bc2ec442fe2b463ad4261b97029124145189f06
Bardia Rasouli <bardiarasouli@protonmail.com>
2025-07-12 18:57:10 +0330

    feat: initial setup with create-next-app

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type { ReactNode, FC, ReactElement } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const RootLayout: FC<Props> = ({ children }): ReactElement => {
+
+  return (
+      <html lang="en" dir="ltr">
+        <body>
+            { children }
+        </body>
+      </html>
+  );
+
+}
+
+export default RootLayout;
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import type { FC, ReactElement } from "react";
+
+const HomePage: FC = (): ReactElement => {
+  return (
+      <>
+        Hello World!
+      </>
+  );
+}
+
+export default HomePage;
